e242d139752102d85025205379ae8ffc010d8b84
“Dawud Ibrahim"Dawud <2617404i@student.gla.ac.uk>
2023-01-26 14:46:17 +0000

    Chapter 6 Complete

--- populate_rango.py
+++ populate_rango.py
@@ -15,25 +15,25 @@ def populate():
 
 	python_pages = [
 	{'title': 'Official Python Tutorial',
-	'url':'http://docs.python.org/3/tutorial/'},
+	'url':'http://docs.python.org/3/tutorial/', 'views':100},
 	{'title':'How to Think like a Computer Scientist',
-	'url':'http://www.greenteapress.com/thinkpython/'},
+	'url':'http://www.greenteapress.com/thinkpython/', 'views':96},
 	{'title':'Learn Python in 10 Minutes',
-	'url':'http://www.korokithakis.net/tutorials/python/'} ]
+	'url':'http://www.korokithakis.net/tutorials/python/', 'views':75} ]
 
 	django_pages = [
 	{'title':'Official Django Tutorial',
-	'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+	'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views':42},
 	{'title':'Django Rocks',
-	'url':'http://www.djangorocks.com/'},
+	'url':'http://www.djangorocks.com/', 'views':57},
 	{'title':'How to Tango with Django',
-	'url':'http://www.tangowithdjango.com/'} ]
+	'url':'http://www.tangowithdjango.com/', 'views':35} ]
 
 	other_pages = [
 	{'title':'Bottle',
-	'url':'http://bottlepy.org/docs/dev/'},
+	'url':'http://bottlepy.org/docs/dev/', 'views':30},
 	{'title':'Flask',
-	'url':'http://flask.pocoo.org'} ]
+	'url':'http://flask.pocoo.org', 'views':18} ]
 
 	cats = {'Python': {'pages': python_pages, 'views':128, 'likes':64},
 	'Django': {'pages': django_pages, 'views':64, 'likes':32},
@@ -47,7 +47,7 @@ def populate():
 	for cat, cat_data in cats.items():
 		c = add_cat(cat, cat_data['views'], cat_data['likes'])
 		for p in cat_data['pages']:
-			add_page(c, p['title'], p['url'])
+			add_page(c, p['title'], p['url'], p['views'])
 
 	# Print out the categories we have added.
 	for c in Category.objects.all():
@@ -55,9 +55,7 @@ def populate():
 			print(f'- {c}: {p}')
 
 def add_page(cat, title, url, views=0):
-	p = Page.objects.get_or_create(category=cat, title=title)[0]
-	p.url=url
-	p.views=views
+	p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
 	p.save()
 	return p
 
